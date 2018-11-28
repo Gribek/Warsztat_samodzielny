0b5b9e2125f41d5c0bafa5f04c589f9a264f305f
Piotr Maslewski <piotrekmaslewski@gmail.com>
2018-11-28 20:20:01 +0100

    Dodałem modele, wykonałem migracje

--- contact_box/models.py
+++ contact_box/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=64)
+    surname = models.CharField(max_length=64, null=True)
+    description = models.TextField(null=True)
+    address = models.ForeignKey("Address", on_delete=models.CASCADE, null=True)
+
+
+class Address(models.Model):
+    city = models.CharField(max_length=64)
+    street = models.CharField(max_length=64)
+    house_number = models.SmallIntegerField()
+    apartment_number = models.SmallIntegerField(null=True)
+
+
+class Phone(models.Model):
+    type = (
+        (1, "prywatny"),
+        (2, "służbowy"),
+        (3, "domowy"),
+        (4, "inny"),
+    )
+    phone_number = models.IntegerField()
+    phone_type = models.SmallIntegerField(choices=type, null=True)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+
+class Email(models.Model):
+    type = (
+        (1, "prywatny"),
+        (2, "służbowy"),
+        (3, "inny"),
+    )
+    email = models.EmailField(max_length=64)
+    email_type = models.SmallIntegerField(choices=type, null=True)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+
+class Groups(models.Model):
+    name = models.CharField(max_length=64)
+    person = models.ManyToManyField(Person)
